581c7a8d7e5712e1a9e0de26c70d62a622bf7b8c
SalmanO7 <msalman221020@gmail.com>
2024-06-13 01:05:36 +0500

    update in reponsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AboutImg from '../../assets/about.jpg';
 import { FaRegAddressCard } from "react-icons/fa6";
-import CV from '../../assets/myCV.pdf';
+import CV from '../../assets/NewCV.pdf';
 import Info from './Info';
 import './About.css';
 
