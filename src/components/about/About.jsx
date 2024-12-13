640c438e5487ce78f6c80238eeeb3a5a7ee7405f
SalmanO7 <msalman221020@gmail.com>
2024-12-14 02:05:06 +0500

    Latest Resume add

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AboutImg from '../../assets/about.jpg';
 import { FaRegAddressCard } from "react-icons/fa6";
-import CV from '../../assets/Muhammad_Salman WebDev_Resume.pdf';
+import CV from '../../assets/Salman_WebDev-Resume.pdf';
 import Info from './Info';
 import './About.css';
 
